bdaef3a0c71d658d433109643f60bfe625a58021
KasenFudge <kasenf@ksu.edu>
2024-07-30 12:53:11 -0500

    First Commit

---
+++ Drakohnon Language Translator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Drakohnon_Language_Translator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Translator Logic/Translator.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace Translator_Logic
+{
+    public class Translator
+    {
+        private string getLetter(char c)
+        {
+            StringBuilder result = new StringBuilder();
+        }
+
+        public string Translate(string s)
+        {
+            s = s.ToUpper();
+            StringBuilder result = new StringBuilder();
+            foreach (char c in s)
+            {
+                int current = (int)c;
+            }
+
+            return result.ToString();
+        }
+    }
+}
